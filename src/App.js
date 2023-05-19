eacf176ac672c62a1460be5ec9daca5264f49c9b
FerozShaik96 <ferozshaik1997@gmail.com>
2023-05-19 23:15:34 +0530

    Rendering Multiple user Details

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
-import AddUser from "./Users/AddUsers";
+import React, { useState } from "react";
+import AddUser from "./Users/AddUser/AddUsers";
+import UserList from "./Users/UserList/UsersList";
 import "./App.css";
 
 function App() {
+  const [userData, setUserData] = useState([]);
+  const UserDataHandler = (data) => {
+    setUserData((prevUserData) => {
+      return [...prevUserData, data];
+    });
+  };
+  console.log(userData);
   return (
     <div>
-      <AddUser />
+      <AddUser onSave={UserDataHandler} />
+      <UserList users={userData} />
     </div>
   );
 }
