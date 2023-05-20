742c49612d17a5f6fb819b25c9973075bb82b2cb
FerozShaik96 <ferozshaik1997@gmail.com>
2023-05-20 12:53:13 +0530

    using ReactDom Portal  and implementing in code

--- src/UI/ErrorModal/ErrorModal.js
+++ src/UI/ErrorModal/ErrorModal.js
@@ -1,24 +1,43 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Card from "../Card/Card";
 import Button from "../Button/Button";
 import Wrapper from "../Wrapper/Wrapper";
 import calsses from "./ErrorModal.module.css";
+const Backdrop = (props) => {
+  return <div className={calsses.backdrop} onClick={props.onConfirm} />;
+};
+const ModalOverLay = (props) => {
+  return (
+    <Card className={calsses.modal}>
+      <header className={calsses.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={calsses.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={calsses.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
 const ErrorModal = (props) => {
   return (
-    <Wrapper>
-      <div className={calsses.backdrop} onClick={props.onConfirm} />
-      <Card className={calsses.modal}>
-        <header className={calsses.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={calsses.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={calsses.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
-    </Wrapper>
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverLay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
   );
 };
 export default ErrorModal;
